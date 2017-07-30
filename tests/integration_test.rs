39f9db857dddf9190fada0171114b5d69793f2db
William Roe <git@wjlr.org.uk>
2017-07-30 23:22:24 +0100

    Cleanup test dir

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -5,8 +5,10 @@ use std::fs;
 
 #[test]
 fn download_test() {
-    env::set_var("CARGO_MANIFEST_DIR", "test_manfest_dir");
+    let test_dir = "test_manfest_dir";
+    env::set_var("CARGO_MANIFEST_DIR", test_dir);
     env::set_var("TARGET", "stable-x86_64-pc-windows-msvc");
-    fs::create_dir_all("test_manfest_dir").expect("create test dir");
+    fs::create_dir_all(test_dir).expect("create test dir");
     assert!(download_sdl2::download().is_ok());
+    fs::remove_dir_all(test_dir).expect("delete test dir");
 }
