c8f3fbf07630a124315fe751b4b868d98c5b0fc1
William Roe <git@wjlr.org.uk>
2017-07-30 21:37:57 +0100

    Run tests on CI

---
+++ tests/integration_test.rs
@@ -0,0 +1,12 @@
+extern crate download_sdl2;
+
+use std::env;
+use std::fs;
+
+#[test]
+fn download_test() {
+    env::set_var("CARGO_MANIFEST_DIR", "test_manfest_dir");
+    env::set_var("TARGET", "stable-x86_64-pc-windows-msvc");
+    fs::create_dir_all("test_manfest_dir").expect("create test dir");
+    assert!(download_sdl2::download().is_ok());
+}
